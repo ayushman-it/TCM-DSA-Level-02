690c0b3b895936edfd411e886c2de6c9c22e291f
Ayushman <avenuedesign768@gmail.com>
2026-09-25 09:10:27 +0530

    Java Intro and Small Code update

---
+++ app.java
@@ -0,0 +1,51 @@
+
+import java.util.concurrent.Flow;
+
+// --Javascript Class 
+// -----||------
+// Java Class
+
+// Javascript
+// let age = 20
+
+// Python 
+// age = 20
+
+// Java
+
+// int age = 20
+// String name = "Rahul";
+
+// Output (Case Sensitive)
+// Container
+class Main{
+    public static void main(String[] args){
+
+        // Data Type
+        // 1. int
+        int age = 50;
+        // 3. String / Char
+        String grade = "A";
+        // 5. boolean
+        boolean stock = true;
+        // 6. double / Flow
+        double result = 45.67;
+
+        // 7. short
+        // 2. byte
+        // 4. long
+
+        System.out.println("Hello");
+        System.out.println("Khushi Developer");
+        System.out.println(age);
+        System.out.println(grade);
+        System.out.println(result);
+        System.out.println(stock);
+    }
+}
+
+// (1) JDK (Code wiriting, Development, comlpile) ----> (2) JVM (JVM help us to execute java codes pn my vertual machine) -----> (3) JRE (Output or Inputs visualize.) 
+
+// JDK = Access of java Code writing 
+// JVM = javac app.java
+// JRE = java Main 
